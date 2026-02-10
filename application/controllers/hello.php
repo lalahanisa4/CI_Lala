f36c3fbe9555a0fb34e174ef8f32386cd1339a68
louis hasashi halim <lalahanisa4@gmail.com>
2026-02-10 15:53:03 +0700

    latihan2

--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -2,4 +2,14 @@
 
 class hello extends CI_Controller {
 
+public function index()
+{
+	$data['nama'] = "Saya!";
+	$this->load->view('hello_view',$data);
+}
+public function namasaya()
+{
+	$data['nama'] = "Lala";
+	$this->load->view('hello_view',$data);
+}
 }
