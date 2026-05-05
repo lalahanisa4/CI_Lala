a860a1bf57bdf595a60fd1e7587031420b9f5c6d
lalahanisa4 <lalahanisa4@gmail.com>
2026-05-05 16:37:53 +0700

    pertemuan10

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -28,6 +28,18 @@
         <span>Anggota</span>
 </a>
 </li>
+<li class="nav-item">
+    <a class="nav-link" href="<?= site_url('buku') ?>">
+        <i class="fas fa-book"></i>
+        <span>Buku</span>
+</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="<?= site_url('peminjaman') ?>">
+        <i class="fas fa-book"></i>
+        <span>Peminjaman</span>
+</a>
+</li>
 <hr class="sidebar-divider d-none d-md-block">
 
 </ul>
